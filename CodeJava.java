761e6dd510776a890b6fb6386d9b4eb219a045c2
Hieununu <khachieu201202@gmail.com>
2023-07-11 15:17:07 +0700

    Update CodeJava.java

--- CodeJava.java
+++ CodeJava.java
@@ -1,2 +1,85 @@
+import java.util.Scanner;
+
 public class CodeJava {
+//public static void main(String[] args){
+//    Scanner input = new Scanner(System.in);
+//    System.out.println("nhap 1 so bat ky tu 0 den 9:");
+//    int a = input.nextInt();
+//    switch(a){
+//        case 0: System.out.println("khong");break;
+//        case 1:System.out.println("mot");break;
+//        case 2:System.out.println("hai");break;
+//        case 3:System.out.println("ba");break;
+//        case 4:System.out.println("bon");break;
+//        case 5:System.out.println("nam");break;
+//        case 6:System.out.println("sau");break;
+//        case 7:System.out.println("bay");break;
+//        case 8:System.out.println("tam");break;
+//        default:
+//            System.out.println("chi co tu 0 den 9 thoi :))) ");
+//    }
+//}
+
+//    public static void main(String[] args)
+//    {
+//        float a, b, c;
+//        System.out.println("nhap 3 so bat ky:");
+//        Scanner sc = new Scanner(System.in);
+//
+//        a = sc.nextFloat();
+//        b = sc.nextFloat();
+//        c = sc.nextFloat();
+//
+//        if(a+b>c && b+c>a && c+a>b)
+//            System.out.println("la 3 canh cua 1 am giac");
+//        else
+//            System.out.println("khong la 3 canh cua 1 tam giac");
+//    }
+
+//    public static void main(String[] args){
+//
+//
+//            float a, b, c;
+//            System.out.println("nhap 3 so bat ky:");
+//            Scanner sc = new Scanner(System.in);
+//
+//            a = sc.nextFloat();
+//            b = sc.nextFloat();
+//            c = sc.nextFloat();
+//
+//            if(a*a+b*b==c*c || b*b+c*c==a*a || c*c+a*a==b*b)
+//                System.out.println("la 3 canh cua tam giac vuong");
+//            else
+//                System.out.println("khong phai 3 canh cua am giac vuong");
+//        }
+
+//    public static void main(String [] args){
+//        int tong = 0;
+//           Scanner sc = new Scanner(System.in);
+//           System.out.println("nhap n= ");
+//           int n = sc.nextInt();
+//           for(int i = 0;i <=n;i++){
+//               tong += i;
+//               System.out.println(tong);
+//           }
+//    }
+
+//    public static void main(String[] args){
+//        int n;
+//        System.out.println("nhap so nguyen:");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for (int i = 1; i <= 20; i++)
+//        {
+//            System.out.println(n + " x " + i + " = " + n*i);
+//        }
+//    }
+
+
+public static void main(String[] args){
+    
 }
+}
+
+
+
